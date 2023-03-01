2f76f6c982b0ad2eced0c5c66bf82525a2bdb1b7
Brendan Miller <bmill3r@gmail.com>
2023-03-01 18:05:49 -0500

    setup vignettes for seqfish, slideseq, spleen and made data rda files

---
+++ R/data.R
@@ -0,0 +1,35 @@
+#' Preprocessed subset of seqFISH metadata of a mouse embryo 
+#'
+#' @format data.frame object where rows are cells,
+#'         `x` and `y` are x-y coordinates,
+#'         `celltypes` are cell type annotations
+#'
+#' @source \url{https://www.nature.com/articles/s41587-021-01006-2}
+#' 
+#' @usage data(seq)
+"seq"
+
+
+#' Preprocessed subset of Slide-seq metadata of a mouse cerebellum.
+#' slideseq.puck190926_08 used in RCTD (https://raw.githack.com/dmcable/spacexr/master/AnalysisPaper/MainFigures/figure4.html)
+#'
+#' @format data.frame object where rows are cells,
+#'         `x` and `y` are x-y coordinates,
+#'         `celltypes` are cell type annotations
+#'
+#' @source \url{https://www.dropbox.com/sh/6klgiz1f9rimzqb/AADX_ziLibO7XY5S4rwPLdQoa/Figure4?dl=0&subfolder_nav_tracking=1}
+#' 
+#' @usage data(slide)
+"slide"
+
+
+#' Preprocessed CODEX metadata of a human spleen.
+#'
+#' @format data.frame object where rows are cells,
+#'         `x` and `y` are x-y coordinates,
+#'         `celltypes` are cell type annotations
+#'
+#' @source \url{https://portal.hubmapconsortium.org/browse/dataset/1393ff7729eff13cd2f4d5d58af96fdb}
+#' 
+#' @usage data(pkhl)
+"pkhl"
