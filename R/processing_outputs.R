344b23410c7781751fd6b0582cc4785bc241737d
Brendan Miller <bmill3r@gmail.com>
2023-03-08 12:09:52 -0500

    updated plotting functions and melting functions and readme

--- R/processing_outputs.R
+++ R/processing_outputs.R
@@ -38,26 +38,28 @@ getNeighbors <- function(cells,
 }
 
 
-#' Melt the output list of findTrendsv2 into a dataframe
+#' Melt the output list of `findTrends()` into a dataframe
 #' 
-#' @description idea is that the output is a list of dataframes, where each dataframe
-#' is for a reference cell type and contains the Z scores at each resolution for the neighbor cells.
+#' @description idea is that the output of `findTrends()` is a list of dataframes, where each dataframe
+#' is for a reference cell type and contains the Z scores at each resolution for the neighbor cell types.
 #' So melt this list of dataframes into a single dataframe. Idea is to get a single dataframe setup for plotting with
-#' ggplot2 and tidyverse functions
+#' ggplot2 and tidyverse functions. `id` parameter allows adding a specific identifier for the given melted results so that
+#' one can combine multiple results dataframes and compare downstream.
+#' For example, you melt a results list from `findTrends()` that was generated from an analysis using a neighbor distance of 100.
+#' Then you generate another one that was done with neighbor distance of 200. 
+#' The id column for each dataframe can be set to "100" and "200", respectively.
+#' Then both dataframes can be combined into one final dataframe.
+#' Now you have identifiers that include: resolution, neighbor, reference, and "id" (ie neighbor distance).
+#' 
 #' @param resultsList list output from findTrendsv2
-#' @param id id desired, can add a column that contains an additional identifier for the results.
-#' For example, you melted a resultsList that was generated from a simulation using 2 circles. 
-#' Then you generate another one that was done with 1 circle. The id column for each dataframe
-#' can be set to "2" and "1". Then both dataframes can be combined into one final dataframe
-#' Now you have identifiers that include: resolution, neighbor, reference, and simulation type.
-#' Can use these for plotting and comparing different things
+#' @param id id desired, can add a column that contains an additional identifier for the results. Can use these for plotting and comparing different things
 #' 
 #' @export
 meltResultsList <- function(resultsList, id = NA){
   
   df <- reshape2::melt(resultsList)
   colnames(df) <- c("resolution", "neighbor", "Z", "reference")
-  ## an identifier for the resultsList analysis
+  ## add an identifier for the particular results
   df[["id"]] <- id
   return(df)
   
