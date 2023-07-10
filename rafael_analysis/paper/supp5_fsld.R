52df2ed9a413eb2d872999b489fe345cfd3dec89
Rafael dos Santos Peixoto <rafaeldossantospeixoto@h186-170.wireless.ufsc.br>
2023-07-10 18:03:57 -0300

    add spleen subset analysis

--- rafael_analysis/paper/supp5_fsld.R
+++ rafael_analysis/paper/supp5_fsld.R
@@ -58,7 +58,16 @@ load("rafael_analysis/paper/supp5_fsld.RData")
 dat <- crawdad::meltResultsList(results, withPerms = TRUE)
 head(dat)
 
-vizAllClusters(cells, fsld$celltypes)
+
+
+## Multiple-test correction
+ntests <- length(unique(dat$reference)) * length(unique(dat$reference))
+psig <- 0.05/ntests
+zsig <- round(qnorm(psig/2, lower.tail = F), 2)
+
+
+
+# vizAllClusters(cells, fsld$celltypes)
 
 ## filter indistinct cells
 dat_filtered <- dat %>% 
@@ -104,6 +113,16 @@ results.subsets <- readRDS("rafael_analysis/paper/supp5_fsld_results.subsets.RDS
 
 
 
+## subsets
+dats <- crawdad::meltResultsList(results.subsets, withPerms = TRUE)
+
+## Multiple-test correction
+ntestss <- length(unique(dats$reference)) * length(unique(dats$neighbor))
+psigs <- 0.05/ntestss
+zsigs <- round(qnorm(psigs/2, lower.tail = F), 2)
+
+
+
 
 # Viz subsetting ----------------------------------------------------------
 
@@ -119,7 +138,7 @@ plt <- crawdad::vizAllClusters(cells = fsld,
                                                  ncol = 1))
 plt
 
-## CD$ Near Fol B
+## CD4 Near Fol B
 ## make a temporary annotation factor with annotations for cells that are of a specific subsets
 annots_temp <- crawdad::selectLabels(df = fsld,
                                      com = fsld$celltypes,
@@ -142,18 +161,18 @@ plt
 # Figure 3f ---------------------------------------------------------------
 
 ## CD4+ memory near follicle B cells colocalize with podoplanin trend
-dats <- crawdad::meltResultsList(results.subsets, withPerms = TRUE)
-
 d1 <- dats[grepl(pattern = "CD4 Memory T cells_near_Fol B cells", 
                  dats$reference) & dats$neighbor %in% c("Fol B cells", "Podoplanin"),]
-plt <- vizTrends(dat = d1, facet = FALSE, id = "neighbor", title = "CD4 Memory T cells near Fol B cells") +
+plt <- vizTrends(dat = d1, facet = FALSE, id = "neighbor", 
+                 title = "CD4 Memory T cells near Fol B cells", 
+                 sig.thresh = zsig) +
   ggplot2::scale_x_log10()
 # ggplot2::theme(legend.position="none")
 plt
 
 dat_filter <- d1 %>% 
   filter(neighbor == 'Podoplanin')
-p <- vizTrends(dat_filter, lines = T, withPerms = T)
+p <- vizTrends(dat_filter, lines = T, withPerms = T, sig.thresh = zsigs)
 p
 pdf('rafael_analysis/paper/supp5_fsld_follB_trend.pdf')
 p 
@@ -165,14 +184,16 @@ dev.off()
 ## CD4+ memory near red bulb colocalize with podoplanin trend
 d2 <- dats[grepl(pattern = "CD4 Memory T cells_near_B cells, red pulp", 
                  dats$reference) & dats$neighbor %in% c("Fol B cells", "Podoplanin"),]
-plt <- vizTrends(dat = d2, facet = FALSE, id = "neighbor", title = "CD4 Memory T cells near Neutrophils/Monocytes") +
+plt <- vizTrends(dat = d2, facet = FALSE, id = "neighbor", 
+                 title = "CD4 Memory T cells near Neutrophils/Monocytes", 
+                 sig.thresh = zsig) +
   ggplot2::scale_x_log10()
 # ggplot2::theme(legend.position="none")
 plt
 
 dat_filter <- d2 %>% 
   filter(neighbor == 'Podoplanin')
-p <- vizTrends(dat_filter, lines = T, withPerms = T)
+p <- vizTrends(dat_filter, lines = T, withPerms = T, sig.thresh = zsigs)
 p
 pdf('rafael_analysis/paper/supp5_fsld_redpulb_trend.pdf')
 p 
@@ -182,14 +203,12 @@ dev.off()
 
 # Figure 3d ---------------------------------------------------------------
 
-p <- vizColocDotplot(dat_filtered, reorder = TRUE) + 
+p <- vizColocDotplot(dat_filtered, reorder = TRUE, zsig.thresh = zsig, zscore.limit = zsig*2) + 
   scale_x_discrete(position = 'bottom') +
   scale_y_discrete(position = 'right') +
   theme(legend.position='bottom',
         axis.text.x = element_text(angle = 90, h = 1),
-        legend.box = 'vertical') +
-  scale_size_continuous(range = c(5, 10), 
-                        trans = 'reverse')
+        legend.box = 'vertical')
 p
 pdf('rafael_analysis/paper/supp5c_fsld.pdf', height = 7.7, width = 6.05)
 p 
@@ -197,15 +216,167 @@ dev.off()
 
 ## reorder according to pkhl
 ct_ordered <- readRDS("rafael_analysis/paper/ct_ordered_spleen.RDS")
-p <- vizColocDotplot(dat_filtered, reorder = TRUE) + 
+p <- vizColocDotplot(dat_filtered, reorder = TRUE, zsig.thresh = zsig, zscore.limit = zsig*2) + 
   scale_x_discrete(position = 'bottom', limits=ct_ordered) +
   scale_y_discrete(position = 'right', limits=ct_ordered) +
   theme(legend.position='bottom',
         axis.text.x = element_text(angle = 90, h = 1),
-        legend.box = 'vertical') +
-  scale_size_continuous(range = c(5, 10), 
-                        trans = 'reverse')
+        legend.box = 'vertical')
 p
 pdf('rafael_analysis/paper/supp5c_fsld_ordered_pkhl.pdf', height = 7.7, width = 6.05)
+p 
+dev.off()
+
+
+
+# Figure 3e ---------------------------------------------------------------
+
+results.subsets <- readRDS("rafael_analysis/paper/supp5_fsld_results.subsets.RDS")
+subset.list <- readRDS("rafael_analysis/paper/supp5_fsld_subset.RDS")
+subcells <- subset.list
+ssample <- fsld
+
+## visualize names
+names(subcells)
+names(subcells)[grepl('CD4 Memory T cells_near', names(subcells))]
+names(subcells)[grepl('Podoplanin_near_', names(subcells))]
+
+## select subsets
+c_cd4_folb <- subcells[["CD4 Memory T cells_near_Fol B cells"]]
+c_cd4_bcell <- subcells[["CD4 Memory T cells_near_B cells, red pulp"]]
+c_podoplanin <- which(ssample$celltypes == "Podoplanin")
+
+## rewrite types
+ssample[c_cd4_folb, ]$celltypes <- 'CD4 Memory T cells_near_Fol B cells'
+ssample[c_cd4_bcell, ]$celltypes <- 'CD4 Memory T cells_near_B cells, red pulp'
+
+## define cts of interest and colors
+cts_interest <- ssample$celltypes %in% c('Podoplanin', 
+                            'CD4 Memory T cells_near_Fol B cells', 
+                            'CD4 Memory T cells_near_B cells, red pulp')
+cols_ssample <- c("#FFFF00", "#FF0080", "#0000FF")
+names(cols_ssample) <-  c('Podoplanin', 
+                          'CD4 Memory T cells_near_Fol B cells', 
+                          'CD4 Memory T cells_near_B cells, red pulp')
+
+df_cts <- ssample %>% 
+  filter(celltypes %in% names(cols_ssample))
+
+df_bkg <-  ssample %>% 
+  filter(!(celltypes %in% names(cols_ssample)))
+
+p <- ggplot() + 
+  geom_point(data = df_bkg, aes(x=x, y=y), 
+             color = 'lightgrey', size=1.5, alpha=0.5) + 
+  
+  geom_point(data = df_cts[df_cts$celltypes == 'Podoplanin', ], 
+             aes(x=x, y=y), color = '#FFFF00', size=1.5, alpha=0.5) + 
+  
+  geom_point(data = df_cts[df_cts$celltypes == 'CD4 Memory T cells_near_Fol B cells', ], 
+             aes(x=x, y=y), color = '#FF0080', size=1.5, alpha=0.5) + 
+  
+  geom_point(data = df_cts[df_cts$celltypes == 'CD4 Memory T cells_near_B cells, red pulp', ], 
+             aes(x=x, y=y), color = '#0000FF', size=1.5, alpha=0.5) + 
+  theme_void() + 
+  theme(legend.position="none")
+p
+png('rafael_analysis/paper/supp5_fsld_spat_cts.png', height = 1024, width = 1024)
+p 
+dev.off()
+
+
+
+
+
+# Replication across samples ----------------------------------------------
+
+## Podoplanin near follicle B cells (white pupl) colocalize with CD4 trend
+d1 <- dats[grepl(pattern = "Podoplanin_near_Fol B cells", 
+                 dats$reference) & dats$neighbor %in% c("CD4 Memory T cells"),]
+plt <- vizTrends(dat = d1, facet = FALSE, id = "neighbor", title = "Podoplanin cells near Fol B cells") +
+  ggplot2::scale_x_log10()
+# ggplot2::theme(legend.position="none")
+plt
+
+dat_filter <- d1 %>% 
+  filter(neighbor == 'CD4 Memory T cells')
+p <- vizTrends(dat_filter, lines = T, withPerms = T, sig.thresh = zsigs)
+p
+pdf('rafael_analysis/paper/replication/fsld_podofolB_CD4.pdf')
+p 
+dev.off()
+
+
+
+## Podoplanin near follicle B cells (red pupl) colocalize with CD4 trend
+d1 <- dats[grepl(pattern = "Podoplanin_near_B cells, red pulp", 
+                 dats$reference) & dats$neighbor %in% c("CD4 Memory T cells"),]
+plt <- vizTrends(dat = d1, facet = FALSE, id = "neighbor", title = "Podoplanin cells near B cells, red pulp") +
+  ggplot2::scale_x_log10()
+# ggplot2::theme(legend.position="none")
+plt
+
+dat_filter <- d1 %>% 
+  filter(neighbor == 'CD4 Memory T cells')
+p <- vizTrends(dat_filter, lines = T, withPerms = T, sig.thresh = zsigs)
+p
+pdf('rafael_analysis/paper/replication/fsld_podoredpulp_CD4.pdf')
+p 
+dev.off()
+
+
+
+# Subsets Podoplanin ------------------------------------------------------
+
+results.subsets <- readRDS("rafael_analysis/paper/supp5_fsld_results.subsets.RDS")
+subset.list <- readRDS("rafael_analysis/paper/supp5_fsld_subset.RDS")
+subcells <- subset.list
+ssample <- fsld
+
+## visualize names
+names(subcells)[grepl('Podoplanin_near_', names(subcells))]
+
+## select subsets
+c_podo_folb <- subcells[["Podoplanin_near_Fol B cells"]]
+c_podo <- which(ssample$celltypes == "Podoplanin")
+c_podo_notfolb <- c_podo[!c_podo %in% c_podo_folb]
+c_cd4 <- which(ssample$celltypes == "CD4 Memory T cells")
+
+## rewrite types
+ssample[c_podo_folb, ]$celltypes <- 'Podoplanin near Fol B cells'
+ssample[c_podo_notfolb, ]$celltypes <- 'Podoplanin not near Fol B cells'
+
+## define cts of interest and colors
+cts_interest <- ssample$celltypes %in% c('CD4 Memory T cells', 
+                                         'Podoplanin near Fol B cells', 
+                                         'Podoplanin not near Fol B cells')
+
+cols_ssample <- c("#FFFF00", "#FF0080", "#0000FF")
+names(cols_ssample) <-  c('CD4 Memory T cells', 
+                          'Podoplanin near Fol B cells', 
+                          'Podoplanin not near Fol B cells')
+
+df_cts <- ssample %>% 
+  filter(celltypes %in% names(cols_ssample))
+
+df_bkg <-  ssample %>% 
+  filter(!(celltypes %in% names(cols_ssample)))
+
+p <- ggplot() + 
+  geom_point(data = df_bkg, aes(x=x, y=y), 
+             color = 'lightgrey', size=1.5, alpha=0.5) + 
+  
+  geom_point(data = df_cts[df_cts$celltypes == 'CD4 Memory T cells', ], 
+             aes(x=x, y=y), color = '#FF0080', size=1.5, alpha=0.5) + 
+  
+  geom_point(data = df_cts[df_cts$celltypes == 'Podoplanin near Fol B cells', ], 
+             aes(x=x, y=y), color = '#FFFF00', size=1.5, alpha=0.5) + 
+  
+  geom_point(data = df_cts[df_cts$celltypes == 'Podoplanin not near Fol B cells', ], 
+             aes(x=x, y=y), color = '#0000FF', size=1.5, alpha=0.5) +
+  theme_void() + 
+  theme(legend.position="none")
+p
+png('rafael_analysis/paper/subsets/podo_fsld_spat_cts.png', height = 1024, width = 1024)
 p 
 dev.off()
--- rafael_analysis/paper/supp5_ksfb.R
+++ rafael_analysis/paper/supp5_ksfb.R
@@ -58,6 +58,15 @@ load("rafael_analysis/paper/supp5_ksfb.RData")
 dat <- crawdad::meltResultsList(results, withPerms = TRUE)
 head(dat)
 
+
+
+## Multiple-test correction
+ntests <- length(unique(dat$reference)) * length(unique(dat$reference))
+psig <- 0.05/ntests
+zsig <- round(qnorm(psig/2, lower.tail = F), 2)
+
+
+
 vizAllClusters(cells, ksfb$celltypes)
 
 ## filter indistinct cells
@@ -104,6 +113,16 @@ results.subsets <- readRDS("rafael_analysis/paper/supp5_ksfb_results.subsets.RDS
 
 
 
+## subsets
+dats <- crawdad::meltResultsList(results.subsets, withPerms = TRUE)
+
+## Multiple-test correction
+ntestss <- length(unique(dats$reference)) * length(unique(dats$neighbor))
+psigs <- 0.05/ntestss
+zsigs <- round(qnorm(psigs/2, lower.tail = F), 2)
+
+
+
 
 # Viz subsetting ----------------------------------------------------------
 
@@ -142,8 +161,6 @@ plt
 # Figure 3f ---------------------------------------------------------------
 
 ## CD4+ memory near follicle B cells colocalize with podoplanin trend
-dats <- crawdad::meltResultsList(results.subsets, withPerms = TRUE)
-
 d1 <- dats[grepl(pattern = "CD4 Memory T cells_near_Fol B cells", 
                  dats$reference) & dats$neighbor %in% c("Fol B cells", "Podoplanin"),]
 plt <- vizTrends(dat = d1, facet = FALSE, id = "neighbor", title = "CD4 Memory T cells near Fol B cells") +
@@ -153,7 +170,7 @@ plt
 
 dat_filter <- d1 %>% 
   filter(neighbor == 'Podoplanin')
-p <- vizTrends(dat_filter, lines = T, withPerms = T)
+p <- vizTrends(dat_filter, lines = T, withPerms = T, sig.thresh = zsigs)
 p
 pdf('rafael_analysis/paper/supp5_ksfb_follB_trend.pdf')
 p 
@@ -172,7 +189,7 @@ plt
 
 dat_filter <- d2 %>% 
   filter(neighbor == 'Podoplanin')
-p <- vizTrends(dat_filter, lines = T, withPerms = T)
+p <- vizTrends(dat_filter, lines = T, withPerms = T, sig.thresh = zsigs)
 p
 pdf('rafael_analysis/paper/supp5_ksfb_redpulb_trend.pdf')
 p 
@@ -182,14 +199,12 @@ dev.off()
 
 # Figure 3d ---------------------------------------------------------------
 
-p <- vizColocDotplot(dat_filtered, reorder = TRUE) + 
+p <- vizColocDotplot(dat_filtered, reorder = TRUE, zsig.thresh = zsig, zscore.limit = zsig*2) + 
   scale_x_discrete(position = 'bottom') +
   scale_y_discrete(position = 'right') +
   theme(legend.position='bottom',
         axis.text.x = element_text(angle = 90, h = 1),
-        legend.box = 'vertical') +
-  scale_size_continuous(range = c(5, 10), 
-                        trans = 'reverse')
+        legend.box = 'vertical')
 p
 pdf('rafael_analysis/paper/supp5c_ksfb.pdf', height = 7.7, width = 6.05)
 p 
@@ -197,15 +212,167 @@ dev.off()
 
 ## reorder according to pkhl
 ct_ordered <- readRDS("rafael_analysis/paper/ct_ordered_spleen.RDS")
-p <- vizColocDotplot(dat_filtered, reorder = TRUE) + 
+p <- vizColocDotplot(dat_filtered, reorder = TRUE, zsig.thresh = zsig, zscore.limit = zsig*2) + 
   scale_x_discrete(position = 'bottom', limits=ct_ordered) +
   scale_y_discrete(position = 'right', limits=ct_ordered) +
   theme(legend.position='bottom',
         axis.text.x = element_text(angle = 90, h = 1),
-        legend.box = 'vertical') +
-  scale_size_continuous(range = c(5, 10), 
-                        trans = 'reverse')
+        legend.box = 'vertical')
 p
 pdf('rafael_analysis/paper/supp5c_ksfb_ordered_pkhl.pdf', height = 7.7, width = 6.05)
+p 
+dev.off()
+
+
+
+# Figure 3e ---------------------------------------------------------------
+
+results.subsets <- readRDS("rafael_analysis/paper/supp5_ksfb_results.subsets.RDS")
+subset.list <- readRDS("rafael_analysis/paper/supp5_ksfb_subset.RDS")
+subcells <- subset.list
+ssample <- ksfb
+
+## visualize names
+names(subcells)
+names(subcells)[grepl('CD4 Memory T cells_near', names(subcells))]
+names(subcells)[grepl('Podoplanin_near_', names(subcells))]
+
+## select subsets
+c_cd4_folb <- subcells[["CD4 Memory T cells_near_Fol B cells"]]
+c_cd4_bcell <- subcells[["CD4 Memory T cells_near_B cells, red pulp"]]
+c_podoplanin <- which(ssample$celltypes == "Podoplanin")
+
+## rewrite types
+ssample[c_cd4_folb, ]$celltypes <- 'CD4 Memory T cells_near_Fol B cells'
+ssample[c_cd4_bcell, ]$celltypes <- 'CD4 Memory T cells_near_B cells, red pulp'
+
+## define cts of interest and colors
+cts_interest <- ssample$celltypes %in% c('Podoplanin', 
+                                         'CD4 Memory T cells_near_Fol B cells', 
+                                         'CD4 Memory T cells_near_B cells, red pulp')
+cols_ssample <- c("#FFFF00", "#FF0080", "#0000FF")
+names(cols_ssample) <-  c('Podoplanin', 
+                          'CD4 Memory T cells_near_Fol B cells', 
+                          'CD4 Memory T cells_near_B cells, red pulp')
+
+df_cts <- ssample %>% 
+  filter(celltypes %in% names(cols_ssample))
+
+df_bkg <-  ssample %>% 
+  filter(!(celltypes %in% names(cols_ssample)))
+
+p <- ggplot() + 
+  geom_point(data = df_bkg, aes(x=x, y=y), 
+             color = 'lightgrey', size=1.5, alpha=0.5) + 
+  
+  geom_point(data = df_cts[df_cts$celltypes == 'Podoplanin', ], 
+             aes(x=x, y=y), color = '#FFFF00', size=1.5, alpha=0.5) + 
+  
+  geom_point(data = df_cts[df_cts$celltypes == 'CD4 Memory T cells_near_Fol B cells', ], 
+             aes(x=x, y=y), color = '#FF0080', size=1.5, alpha=0.5) + 
+  
+  geom_point(data = df_cts[df_cts$celltypes == 'CD4 Memory T cells_near_B cells, red pulp', ], 
+             aes(x=x, y=y), color = '#0000FF', size=1.5, alpha=0.5) + 
+  theme_void() + 
+  theme(legend.position="none")
+p
+png('rafael_analysis/paper/supp5_ksfb_spat_cts.png', height = 1024, width = 1024)
+p 
+dev.off()
+
+
+
+
+
+# Replication across samples ----------------------------------------------
+
+## Podoplanin near follicle B cells (white pupl) colocalize with CD4 trend
+d1 <- dats[grepl(pattern = "Podoplanin_near_Fol B cells", 
+                 dats$reference) & dats$neighbor %in% c("CD4 Memory T cells"),]
+plt <- vizTrends(dat = d1, facet = FALSE, id = "neighbor", title = "Podoplanin cells near Fol B cells") +
+  ggplot2::scale_x_log10()
+# ggplot2::theme(legend.position="none")
+plt
+
+dat_filter <- d1 %>% 
+  filter(neighbor == 'CD4 Memory T cells')
+p <- vizTrends(dat_filter, lines = T, withPerms = T, sig.thresh = zsigs)
+p
+pdf('rafael_analysis/paper/replication/ksfb_podofolB_CD4.pdf')
+p 
+dev.off()
+
+
+
+## Podoplanin near follicle B cells (red pupl) colocalize with CD4 trend
+d1 <- dats[grepl(pattern = "Podoplanin_near_B cells, red pulp", 
+                 dats$reference) & dats$neighbor %in% c("CD4 Memory T cells"),]
+plt <- vizTrends(dat = d1, facet = FALSE, id = "neighbor", title = "Podoplanin cells near B cells, red pulp") +
+  ggplot2::scale_x_log10()
+# ggplot2::theme(legend.position="none")
+plt
+
+dat_filter <- d1 %>% 
+  filter(neighbor == 'CD4 Memory T cells')
+p <- vizTrends(dat_filter, lines = T, withPerms = T, sig.thresh = zsigs)
+p
+pdf('rafael_analysis/paper/replication/ksfb_podoredpulp_CD4.pdf')
+p 
+dev.off()
+
+
+
+# Subsets Podoplanin ------------------------------------------------------
+
+results.subsets <- readRDS("rafael_analysis/paper/supp5_ksfb_results.subsets.RDS")
+subset.list <- readRDS("rafael_analysis/paper/supp5_ksfb_subset.RDS")
+subcells <- subset.list
+ssample <- ksfb
+
+## visualize names
+names(subcells)[grepl('Podoplanin_near_', names(subcells))]
+
+## select subsets
+c_podo_folb <- subcells[["Podoplanin_near_Fol B cells"]]
+c_podo <- which(ssample$celltypes == "Podoplanin")
+c_podo_notfolb <- c_podo[!c_podo %in% c_podo_folb]
+c_cd4 <- which(ssample$celltypes == "CD4 Memory T cells")
+
+## rewrite types
+ssample[c_podo_folb, ]$celltypes <- 'Podoplanin near Fol B cells'
+ssample[c_podo_notfolb, ]$celltypes <- 'Podoplanin not near Fol B cells'
+
+## define cts of interest and colors
+cts_interest <- ssample$celltypes %in% c('CD4 Memory T cells', 
+                                         'Podoplanin near Fol B cells', 
+                                         'Podoplanin not near Fol B cells')
+
+cols_ssample <- c("#FFFF00", "#FF0080", "#0000FF")
+names(cols_ssample) <-  c('CD4 Memory T cells', 
+                          'Podoplanin near Fol B cells', 
+                          'Podoplanin not near Fol B cells')
+
+df_cts <- ssample %>% 
+  filter(celltypes %in% names(cols_ssample))
+
+df_bkg <-  ssample %>% 
+  filter(!(celltypes %in% names(cols_ssample)))
+
+p <- ggplot() + 
+  geom_point(data = df_bkg, aes(x=x, y=y), 
+             color = 'lightgrey', size=1.5, alpha=0.5) + 
+  
+  geom_point(data = df_cts[df_cts$celltypes == 'CD4 Memory T cells', ], 
+             aes(x=x, y=y), color = '#FF0080', size=1.5, alpha=0.5) + 
+  
+  geom_point(data = df_cts[df_cts$celltypes == 'Podoplanin near Fol B cells', ], 
+             aes(x=x, y=y), color = '#FFFF00', size=1.5, alpha=0.5) + 
+  
+  geom_point(data = df_cts[df_cts$celltypes == 'Podoplanin not near Fol B cells', ], 
+             aes(x=x, y=y), color = '#0000FF', size=1.5, alpha=0.5) +
+  theme_void() + 
+  theme(legend.position="none")
+p
+png('rafael_analysis/paper/subsets/podo_ksfb_spat_cts.png', height = 1024, width = 1024)
 p 
 dev.off()
