3779e5518ddf5502194236b94f0a4cf551c1436a
Rafael dos Santos Peixoto <rafaeldossantospeixoto@gmail.com>
2023-10-23 16:10:41 -0400

    fix toSP example

--- R/processing_inputs.R
+++ R/processing_inputs.R
@@ -7,12 +7,57 @@
 #' @examples 
 #' \dontrun{
 #' data(sim)
-#' cells <- toSP(pos = sim[,c("x", "y")], celltypes = slide$type)
+#' cells <- toSP(pos = sim[,c("x", "y")], celltypes = sim$type)
 #' }
 #' 
 #' @export
 toSP <- function(pos, celltypes, verbose=TRUE){
+  .Deprecated("toSF")
+  if(length(levels(celltypes)) == 0){
+    message("Warning: 'celltypes' does not have levels. Creating levels from values")
+    celltypes <- factor(celltypes)
+    names(celltypes) <- rownames(pos)
+  }
   
+  if(verbose){
+    message("creating `sp::SpatialPointsDataFrame`")
+  }
+  
+  cells <- sp::SpatialPointsDataFrame(
+    coords = as.data.frame(pos),
+    data = data.frame(
+      celltypes = celltypes
+      # name = rownames(pos)
+    ))
+  cells <- sf::st_as_sf(cells)
+  
+  ## Change rowname assignments of cells to integers.
+  ## Solution to keep rows in same order later on when
+  ## randomly shuffling cell labels
+  rownames(cells) <- as.character(1:dim(cells)[1])
+  
+  # make asumption that cell type attribute is constant throughout the geometries of each cell
+  ## it removed the warning that keep popping up, which says this assumption is made anyways
+  sf::st_agr(cells) <- "constant"
+  
+  return(cells)
+}
+
+#' Go from positions and celltype annotations to spatialpointsdataframe object
+#' 
+#' @param pos data frame; x and y coordinates of each cell
+#' @param celltypes character vector; the cell type of each cell provided in pos
+#' @param verbose Boolean; verbosity (default TRUE)
+#' 
+#' @examples 
+#' \dontrun{
+#' data(sim)
+#' cells <- toSP(pos = sim[,c("x", "y")], celltypes = sim$type)
+#' }
+#' 
+#' @export
+toSP <- function(pos, celltypes, verbose=TRUE){
+  .Deprecated("toSF")
   if(length(levels(celltypes)) == 0){
     message("Warning: 'celltypes' does not have levels. Creating levels from values")
     celltypes <- factor(celltypes)
