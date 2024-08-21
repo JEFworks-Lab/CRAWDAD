8f82e1cd70ae465d38aa52d4104a8abb0ce8ed04
Rafael dos Santos Peixoto <rafaeldossantospeixoto@gmail.com>
2024-08-21 12:03:19 -0400

    add vizGrids function

--- R/visualization.R
+++ R/visualization.R
@@ -218,6 +218,74 @@ vizEachCluster <- function(cells, coms, axisAdj = 1, s = 0.5, a = 1,
 }
 
 
+
+#' Visualize grids and clusters
+#' 
+#' @description Uses the cells sf object and size of grid to visualize the grids 
+#' used to cheate the null background.
+#' 
+#' @param cells sf object; spatial (x and y) coordinates and celltypes column
+#' @param gridSize numeric; size of the grid to plot
+#' @param square boolean; if true, create a squared grid, if false, make 
+#' hexagonal grid (default TRUE)
+#' @param ofInterest character vector; a vector of specific clusters to visualize
+#' @param pointSize numeric; size of points
+#' @param alpha numeric; transparency of points
+#' 
+#' @return plot
+#' 
+#' @examples
+#' \dontrun{
+#' data(slide)
+#' cells <- crawdad::toSF(pos = slide[,c("x", "y")], celltypes = slide$celltypes)
+#' vizAllClusters(cells)
+#' }
+#' 
+#' @export
+vizGrids <- function(cells, scale,
+                     permutation = 1, totalPermutations = NULL, 
+                     square = TRUE,
+                     ofInterest = NULL, pointSize = 1, alpha = 0.5){
+  
+  ## the total number of permutations is used to calculate the offset, so if the
+  ## user asks for a different permutation, but does not provide it, the code
+  ## will not work
+  if ((permutation != 1) & (is.null(totalPermutations))){
+    stop('provide the total number of permutations')
+  }
+  
+  ## create grids
+  if (permutation == 1) {
+    ## for no permutations 
+    ## create grid with no offset
+    grid <- sf::st_make_grid(cells, cellsize = scale, square = square)
+  } else {
+    ## for permutations 
+    ## calculate offset
+    offset <- -seq(from = 0, to = scale, by = scale/totalPermutations)[permutation]
+    ## get bounding box with min and max coordinates
+    bbox <- sf::st_bbox(cells$geometry)
+    ## create grid with offset
+    grid <- sf::st_make_grid(cells, cellsize = scale, 
+                             offset = c(bbox[['xmin']] + offset, 
+                                        bbox[['ymin']] + offset),
+                             square = square)
+  }
+  
+  
+  ## get the coordinates of the centers of the grid tiles to add the tile IDs
+  grid_coords_centroids <- as.data.frame(sf::st_coordinates(sf::st_centroid(grid)))
+  grid_coords_centroids$name <- as.character(rownames(grid_coords_centroids))
+  ## create plot
+  crawdad::vizClusters(cells = cells, ofInterest, pointSize, alpha) + 
+    ## add in the grid information on top of the plot
+    ggplot2::geom_sf(data = grid, fill = NA) +
+    ggplot2::geom_text(data = grid_coords_centroids, 
+                       ggplot2::aes(X, Y, label = name))
+}
+
+
+
 #' Plot trends with ggplot2
 #' 
 #' @description The input data.frame should be the results list from `findTrends()` that has been melted into a data.frame using `meltResultsList()`.
